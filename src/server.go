a9e01e3accec8da6d317085429eda65890b0e48d
Frosyka <narp_kik@mail.ru>
2025-09-15 14:41:38 +0300

    Changed server.go; deleted useless handlers

--- src/server.go
+++ src/server.go
@@ -11,41 +11,14 @@ import (
 	"time"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the home page!")
-}
-
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the about page.")
-}
-
 func readyHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "OK")
+	log.Println("/ready has been called. Server is ok")
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "READY")
-}
-
-func formHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		fmt.Fprintf(w, `
-            <form method="POST">
-                <input type="text" name="name" placeholder="Enter your name">
-                <button type="submit">Submit</button>
-            </form>
-        `)
-	case http.MethodPost:
-		err := r.ParseForm()
-		if err != nil {
-			fmt.Fprintf(w, "Error parsing form: %v", err)
-			return
-		}
-		name := r.FormValue("name")
-		fmt.Fprintf(w, "Hello, %s!", name)
-	}
+	log.Println("/health has been called. Server is ready")
 }
 
 func gracefulShutdown(server *http.Server) {
@@ -66,17 +39,14 @@ func gracefulShutdown(server *http.Server) {
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/about", aboutHandler)
 	http.HandleFunc("/ready", readyHandler)
 	http.HandleFunc("/health", healthHandler)
-	http.HandleFunc("/form", formHandler)
 
-	server := &http.Server{Addr: ":8080"}
+	server := &http.Server{Addr: ":8092"}
 
 	go gracefulShutdown(server)
 
-	log.Println("Starting server at port 8080")
+	log.Println("Starting server at port 8092")
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		log.Fatalf("could not listen on %s: %v\n", server.Addr, err)
 	}
